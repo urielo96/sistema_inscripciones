3236967c36429c6375e32284fa18768ebe45c13b
Urielo <diegouriel96@hotmail.com>
2023-06-30 18:58:10 -0600

    ¨Primer avanze

---
+++ inscripcion/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Alumno)
+admin.site.register(Grupo)
+admin.site.register(Asignatura)
+admin.site.register(Inscripcion)
---
+++ inscripcion/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+
+class Alumno(models.Model):
+    numero_cuenta = models.IntegerField(primary_key= True)
+    nombre = models.CharField(max_length=50, help_text= "Nombre o nombres")
+    apellido_paterno = models.CharField(max_length=50, help_text= "Apellido Paterno")
+    apellido_materno = models.CharField(max_length=50, help_text= "Apellido Materno") 
+    edad = models.IntegerField(help_text="Edad")
+    fecha_ingreso = models.DateField()
+    semestre_actual = models.CharField(max_length=20)
+    genero = [
+        ('F', 'Femenino'),
+        ('M', 'Masculino'),
+        ('O', 'Otro'),]
+    genero = models.CharField(max_length=1, choices=genero) 
+    foto = models.ImageField(upload_to='fotos_alumnos/')
+    
+
+    def __str__(self):
+        nombre_completo = "{0} {1} {2}"
+        return nombre_completo.format(self.nombre,self.apellido_paterno,self.apellido_materno)
+    
+    
+
+class Asignatura(models.Model):
+    clave_asignatura = models.SmallIntegerField(primary_key=True)
+    denominacion = models.CharField(max_length=80)
+    semestre = models.CharField(max_length=50)
+    creditos = models.PositiveIntegerField(default=None)
+    
+    def __str__(self):
+        return self.denominacion
+    
+    
+
+
+
+
+class Grupo(models.Model):
+    clave_grupo = models.SmallIntegerField(primary_key= True)
+    descripcion = models.CharField(max_length=100)
+    asignaturas = models.ManyToManyField(Asignatura)
+
+    def __str__(self):
+        return str(self.clave_grupo)
+    
+
+
+
+class Inscripcion(models.Model):
+    id = models.AutoField
+    numero_cuenta = models.ForeignKey(Alumno, null=False, blank=False, on_delete=models.CASCADE)
+    asignatura = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        numero_de_cuenta = "{0}"
+        print(numero_de_cuenta)
+        return numero_de_cuenta.format(self.numero_cuenta)
+
+
+
+    
+
+    
+    
+    
+
+    
+   
+    
+
